b72836ffbd35077396cc060a4b01f6b8803343a3
VIVEK PANDA <vivekpanda273@gmail.com>
2022-06-28 09:39:37 +0530

    Update PythonApplication4.py

--- PythonApplication4.py
+++ PythonApplication4.py
@@ -1,4 +1,3 @@
-
 import pyodbc
 import PySimpleGUI as sg
 
@@ -7,10 +6,10 @@ conn = pyodbc.connect("Driver={SQL Server};"
                       "Database=LoginDB;"
                       "Trusted_Connection=yes;")
 sg.theme('Python')
-layout = [[sg.Text('Name'), sg.Input()],
-          [sg.Text('Mobile Number'), sg.Input()],
-          [sg.Text('Address'), sg.Input()],
-          [sg.Button('Save'), sg.Button('Cancel')]]
+layout = [[sg.Text('Name'), sg.Input(key='NAME'),sg.Push(),],
+          [sg.Text('Mobile Number'), sg.Input(key='MOBILE_NUMBER'),sg.Push()],
+          [sg.Text('Address'), sg.Input(key='ADDRESS'),sg.Push()],
+          [sg.Button('Save'), sg.Button('Cancel'),sg.Button('Search'),sg.Button('Delete'),sg.Button('Update')]]
 Window = sg.Window('User Data Entry',layout, element_justification='center')
 while True:
     event, values = Window.read()
@@ -18,17 +17,43 @@ while True:
          break
     elif event == 'Save':
          
-             Name= values[0]
-             Mobile_Number= values[1]
-             Address=values[2]
              cursor =conn.cursor()
-             sql="INSERT INTO LoginTable(Name,Mobile_Number,Address) VALUES(?,?,?);"
-             cursor.execute(sql,(Name,Mobile_Number,Address))
+             query="EXEC [insertion] @Name= ?, @Mobile_Number= ? , @Address= ? "
+             cursor.execute(query,values['NAME'],values['MOBILE_NUMBER'],values['ADDRESS'])
              cursor.commit()
              sg.popup('Data Saved')
              cursor.execute("SELECT * FROM LoginTable")
              for row in cursor:
-                print('row = %r' % (row))
+                print('row=%r' % (row))
+    elif event == 'Search':
+        cursor =conn.cursor()
+        sql="EXEC[Searching] @Mobile_Number=?"
+        cursor.execute(sql,values['MOBILE_NUMBER'])
+        sg.popup('Data Found')
+        records=cursor.fetchall()
+        for record in records:
+            Window['NAME'].update(record[0])
+            Window['ADDRESS'].update(record[2])
+            cursor.commit()
+    elif event =='Delete':
+        cursor= conn.cursor()
+        sql="EXEC [Deletion] @Name=?;"
+        cursor.execute(sql,values['NAME'])
+        cursor.commit()
+        sg.popup('Entered data has been deleted')
+        cursor.execute("SELECT * FROM LoginTable")
+        for row in cursor:
+            print('row=%r' % (row))
+    elif event=='Update':
+        cursor=conn.cursor()
+        sql="EXEC[UPDATING] @Name=?,@Mobile_Number=?,@Address=?;"
+        cursor.execute(sql,values['NAME'],values['MOBILE_NUMBER'],values['ADDRESS'])
+        cursor.commit()
+        sg.popup('Data has been Updated')
+        cursor.execute("SELECT* FROM LoginTable")
+        for row in cursor:
+            print('row=%r' % (row))
+
 
 
 
